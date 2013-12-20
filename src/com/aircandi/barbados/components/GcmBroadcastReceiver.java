1948c7cb4b8fbc9cc8c88224f857af852bf558c7
Jay Massena <jay@3meters.com>
2013-12-20 15:56:59 -0800

    bug fixes.

--- src/com/aircandi/barbados/components/GcmBroadcastReceiver.java
+++ src/com/aircandi/barbados/components/GcmBroadcastReceiver.java
@@ -5,7 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.aircandi.barbados.R;
+import com.aircandi.R;
 import com.aircandi.utilities.Strings;
 
 /**
